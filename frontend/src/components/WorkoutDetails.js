c01609afdd709aa51f1a14432cfa4f5012ede915
ruxeyy04 <ruxepasok356@gmail.com>
2024-07-20 22:28:09 +0800

    Delete

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,13 +1,28 @@
-const WorkoutDetails = ({ workout, workid }) => {
-    return (
-      <div className="workout-details">
-        <h4>{workout.title}</h4>
-        <p><strong>Load (kg): </strong>{workout.load}</p>
-        <p><strong>Number of reps: </strong>{workout.reps}</p>
-        <p>{workout.createdAt}</p>
-        <p>{workout.createdAt}</p>
-      </div>
-    )
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
   }
-  
-  export default WorkoutDetails
+
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p><strong>Load (kg): </strong>{workout.load}</p>
+      <p><strong>Number of reps: </strong>{workout.reps}</p>
+      <p>{workout.createdAt}</p>
+      <span onClick={handleClick}>X</span>
+    </div>
+  )
+}
+
+export default WorkoutDetails
