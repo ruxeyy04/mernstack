7dc709193953ecae817cad79e8bbf6849d305efc
ruxeyy04 <ruxepasok356@gmail.com>
2024-07-20 21:34:06 +0800

    First commit Suicide

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const wrkroute = require("./routes/workouts");
+//express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", wrkroute);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening for requests on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("There is an error:", error);
+  });
